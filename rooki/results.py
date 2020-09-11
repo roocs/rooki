6d6599a785729ffe0d24ddd76666a426451cc945
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-11 18:40:02 +0200

    show xml document

--- rooki/results.py
+++ rooki/results.py
@@ -1,9 +1,18 @@
+import requests
+
+
 class Result(object):
     def __init__(self, response):
         self.response = response
 
-    def reference(self):
+    @property
+    def url(self):
         return self.response.get()[0]
+
+    @property
+    def xml(self):
+        xml = requests.get(self.url).text
+        return xml
 
     def download_urls(self):
         return []
