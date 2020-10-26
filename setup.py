48bfcc65324bc25b592538277d5a182feb45be28
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-10-26 20:30:45 +0100

    bumpversion v0.2.0

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ __author__ = "Carsten Ehbrecht"
 __contact__ = "ehbrecht@dkrz.de"
 __copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
-__version__ = "0.1.0"
-
-from setuptools import setup, find_packages
+__version__ = "0.2.0"
 
 # One strategy for storing the overall version is to put it in the top-level
 # package's __init__ but Nb. __init__.py files are not needed to declare
@@ -27,51 +25,52 @@ with open("README.rst") as readme_file:
 
 
 requirements = [line.strip() for line in open("requirements.txt")]
-test_requirements = ['pytest', ]
+test_requirements = [
+    "pytest",
+]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
 
 setup(
     author=__author__,
     author_email=__contact__,
-
     # See:
     # https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Security',
-        'Topic :: Internet',
-        'Topic :: Scientific/Engineering',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: System :: Systems Administration :: Authentication/Directory',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Security",
+        "Topic :: Internet",
+        "Topic :: Scientific/Engineering",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     description="A client for roocs climate data operations service.",
-
     license=__license__,
-
     # This qualifier can be used to selectively exclude Python versions -
     # in this case early Python 2 and 3 releases
-    python_requires='>=3.7.0',
+    python_requires=">=3.7.0",
     entry_points={
-        'console_scripts': [
-            'rooki=rooki.cli:main',
+        "console_scripts": [
+            "rooki=rooki.cli:main",
         ],
     },
     install_requires=[
@@ -79,16 +78,15 @@ setup(
         # 'metalink @ git+https://github.com/metalink-dev/pymetalink.git',
     ],
     long_description=_long_description,
-    long_description_content_type='text/x-rst',
-
+    long_description_content_type="text/x-rst",
     include_package_data=True,
-    keywords='rooki',
-    name='rooki',
-    packages=find_packages(include=['rooki']),
+    keywords="rooki",
+    name="rooki",
+    packages=find_packages(include=["rooki"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/roocs/rooki',
+    url="https://github.com/roocs/rooki",
     version=__version__,
     zip_safe=False,
 )
